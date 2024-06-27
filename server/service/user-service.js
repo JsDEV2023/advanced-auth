c16e0d81ab3a875a85052a3a3fb98beb853f2d89
JsDEV2023 <schura.ankudinov@gmail.com>
2024-06-27 14:08:53 +0500

    create user-dto

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -2,6 +2,7 @@ const UserModel = require('../models/user-model')
 const bcrypt = require('bcrypt')
 const uuid = require('uuid')
 const mailService = require('./mail-service')
+const tokenService = require('./token-service')
 
 class UserService {
     async registration (email, password) {
@@ -13,6 +14,7 @@ class UserService {
         const activationLink = uuid.v4()
         const user = await UserModel.create({email, password: hashPassword, activationLink})
         await mailService.sendActivationMail(emial, activationLink)
+        const tokens = tokenService.generateTokens()
     }
 }
 
